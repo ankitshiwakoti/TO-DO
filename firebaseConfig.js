fc5737bf63f93929ef0c429cc0369cc6ba780841
Ankit Shiwakoti <51996000+ankitshiwakoti@users.noreply.github.com>
2025-03-19 11:32:29 -0500

    Refactor Firebase configuration and enhance task management UI with improved styling and functionality

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,23 @@
 
- const firebaseConfig = {
-    apiKey: "AIzaSyA6gT7w-ZfnpMzDuTaQ6orF9lKp8HpQj0I",
-    authDomain: "to-do-d08ad.firebaseapp.com",
-    projectId: "to-do-d08ad",
-    storageBucket: "to-do-d08ad.appspot.com",
-    messagingSenderId: "876562316515",
-    appId: "1:876562316515:web:31db7dc22c91b187b199d6"
-  };
+//  const firebaseConfig = {
+//     apiKey: "AIzaSyA6gT7w-ZfnpMzDuTaQ6orF9lKp8HpQj0I",
+//     authDomain: "to-do-d08ad.firebaseapp.com",
+//     projectId: "to-do-d08ad",
+//     storageBucket: "to-do-d08ad.appspot.com",
+//     messagingSenderId: "876562316515",
+//     appId: "1:876562316515:web:31db7dc22c91b187b199d6"
+//   };
 
 
-  window.app = firebase.initializeApp(firebaseConfig);
+//   window.app = firebase.initializeApp(firebaseConfig);
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA6gT7w-ZfnpMzDuTaQ6orF9lKp8HpQj0I",
+  authDomain: "to-do-d08ad.firebaseapp.com",
+  projectId: "to-do-d08ad",
+  storageBucket: "to-do-d08ad.appspot.com",
+  messagingSenderId: "876562316515",
+  appId: "1:876562316515:web:31db7dc22c91b187b199d6"
+};
+
+window.app = firebase.initializeApp(firebaseConfig);
